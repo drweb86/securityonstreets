629baf23a8f4121dc29f65cccf11a7b9797b07f4
Siarhei Kuchuk <4712173+drweb86@users.noreply.github.com>
2012-07-17 21:53:11 +0000

    * reading server configuration file.\

--- src/Cpp/MessageRouter/Server/ConsoleServer/MessageRouterConfiguration.cpp
+++ src/Cpp/MessageRouter/Server/ConsoleServer/MessageRouterConfiguration.cpp
@@ -1,17 +1,13 @@
-//
-// mime_types.hpp
-// ~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2012 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
 //MS-BUG workaround
 #include <cstdio>
 #include <boost/filesystem.hpp>
+#include <iostream>
+#include <fstream>
 #include "MessageRouterConfiguration.hpp"
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/foreach.hpp>
+#include <boost/foreach_fwd.hpp>
 
 Configuration::MessageRouterConfiguration::MessageRouterConfiguration()
 {
@@ -25,9 +21,67 @@ void Configuration::MessageRouterConfiguration::Load()
 		throw new std::exception("Configuration file 'MessageBus.xml' is missing.");
 	}
 
-	//TODO: read file
+	// populate tree structure pt
+	using boost::property_tree::ptree;
+	ptree configurationFileTree;
+	boost::property_tree::read_xml(_expectedFileName, configurationFileTree);
+	
+	ptree configurationNode = configurationFileTree.get_child("configuration");
+	
+	ptree messagingNode = configurationNode.get_child("messaging");
+	
+	ptree producersNode = messagingNode.get_child("producers");
+	ptree subscribersNode = messagingNode.get_child("subscribers");
 
-	//TODO: add configuration nodes
+	BOOST_FOREACH( ptree::value_type const& producer, producersNode ) 
+	{
+		if( producer.first == "host" ) 
+		{
+			_allowedProducers.push_back (producer.second.get<std::string>("<xmlattr>.name"));
+	    }
+	}
+
+	BOOST_FOREACH( ptree::value_type const& subscriber, subscribersNode ) 
+	{
+		if( subscriber.first == "host" ) 
+		{
+			_allowedSubscribers.push_back (subscriber.second.get<std::string>("<xmlattr>.name"));
+	    }
+	}
+
+	ptree serverConfigurationNode = configurationNode.get_child("server");
+
+	_serverAddress = serverConfigurationNode.get<std::string>("<xmlattr>.address");
+	_serverPort = serverConfigurationNode.get<int>("<xmlattr>.port");
+	_serverThreads = serverConfigurationNode.get<int>("<xmlattr>.threads");
+}
+
+void Configuration::MessageRouterConfiguration::ConfigurationToConsole ()
+{
+	std::cout << "\nAllowed subscriber hosts:\n";
+	for (int i = 0; i < _allowedSubscribers.size(); i++)
+	{
+		std::cout << " - ";
+		std::cout << _allowedSubscribers[i];
+		std::cout << "\n";
+	}
+	
+	std::cout << "\nAllowed producer hosts:\n";
+	for (int i = 0; i < _allowedProducers.size(); i++)
+	{
+		std::cout << " - ";
+		std::cout << _allowedProducers[i];
+		std::cout << "\n";
+	}
+
+	std::cout << "\nServer setup:";
+	std::cout << "\nHost : ";
+	std::cout << _serverAddress;
+	std::cout << "\nPort : ";
+	std::cout << _serverPort;
+	std::cout << "\nThreads : ";
+	std::cout << _serverThreads;
+	std::cout << "\n";
 }
 
 bool Configuration::MessageRouterConfiguration::MessageProducerHostAllowed(char* host)
